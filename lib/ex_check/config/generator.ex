9cebc34b1523a82ee6044467e9759bd516180e88
Karol Słuszniak <ksluszniak@gmail.com>
2019-09-12 15:08:20 +0200

    Adjust generated config for new changes

--- lib/ex_check/config/generator.ex
+++ lib/ex_check/config/generator.ex
@@ -9,21 +9,23 @@ defmodule ExCheck.Config.Generator do
     # parallel: true,
     # skipped: true,
 
-    ## list of tools (see `mix check` docs for defaults)
+    ## list of tools (see `mix check` docs for a list of default curated tools)
     tools: [
       ## curated tools may be disabled (e.g. the check for compilation warnings)
       # {:compiler, false},
 
-      ## ...or adjusted (e.g. use one-line formatter for more compact credo output)
-      # {:credo, "mix credo --format oneline"},
+      ## ...or have command & args adjusted (e.g. enable skip comments for sobelow)
+      # {:sobelow, "mix sobelow --exit --skip"},
 
-      ## ...or reordered (e.g. to see output from ex_unit before others)
-      # {:ex_unit, order: -1},
+      ## ...or reordered (e.g. to see output from dialyzer before others)
+      # {:dialyzer, order: -1},
 
-      ## custom new tools may be added (mix tasks or arbitrary commands)
-      # {:my_mix_task, command: "mix release", env: %{"MIX_ENV" => "prod"}},
-      # {:my_arbitrary_tool, command: "npm test", cd: "assets"},
-      # {:my_arbitrary_script, command: ["my_script", "argument with spaces"], cd: "scripts"}
+      ## ...or reconfigured (e.g. disable parallel execution of ex_unit in umbrella)
+      # {:ex_unit, umbrella: [parallel: false]},
+
+      ## custom new tools may be added (Mix tasks or arbitrary commands)
+      # {:my_task, "mix my_task", env: %{"MIX_ENV" => "prod"}},
+      # {:my_tool, ["my_tool", "arg with spaces"]}
     ]
   ]
   """
