b1e493fc43e46c24049e8c60c2f9a67a482b563b
Grant McLendon <grant@7mind.de>
2022-11-18 10:43:08 -0600

    Fix test regressions for Elixir v1.7
    ex_doc  >= v0.23.0 requires Elixir >v1.10

--- test/ex_check/project_cases/external_tools_test.exs
+++ test/ex_check/project_cases/external_tools_test.exs
@@ -2,7 +2,14 @@ defmodule ExCheck.ProjectCases.ExternalToolsTest do
   use ExCheck.ProjectCase, async: true
 
   test "external tools (except dialyzer)", %{project_dir: project_dir} do
-    set_mix_deps(project_dir, [:ex_check, :credo, :doctor, :ex_doc, :sobelow, :mix_audit])
+    tools =
+      if Version.match?(System.version(), "< 1.10.0") do
+        [:ex_check, :credo, :doctor, :sobelow, :mix_audit]
+      else
+        [:ex_check, :credo, :doctor, :ex_doc, :sobelow, :mix_audit]
+      end
+
+    set_mix_deps(project_dir, tools)
 
     assert {output, 0} = System.cmd("mix", ~w[check], cd: project_dir, env: %{"MIX_ENV" => "dev"})
 
@@ -13,11 +20,18 @@ defmodule ExCheck.ProjectCases.ExternalToolsTest do
 
     if Version.match?(System.version(), ">= 1.8.0") do
       assert output =~ "doctor success"
+    else
+      assert output =~ "doctor skipped"
+    end
+
+    if Version.match?(System.version(), ">= 1.10.0") do
+      assert output =~ "ex_doc success"
+    else
+      assert output =~ "ex_doc skipped due to missing package ex_doc"
     end
 
     assert output =~ "sobelow success"
     assert output =~ "dialyzer skipped due to missing package dialyxir"
-    assert output =~ "ex_doc success"
     assert output =~ "mix_audit success"
   end
 end
