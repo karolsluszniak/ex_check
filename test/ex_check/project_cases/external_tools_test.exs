923b9e11bf42caa17f9bac85bae5c8b28950f8ce
Karol Słuszniak <ksluszniak@gmail.com>
2022-12-05 12:24:43 +0100

    Fix further CI-failing tests

--- test/ex_check/project_cases/external_tools_test.exs
+++ test/ex_check/project_cases/external_tools_test.exs
@@ -11,8 +11,7 @@ defmodule ExCheck.ProjectCases.ExternalToolsTest do
 
     set_mix_deps(project_dir, tools)
 
-    assert {output, 0} =
-             System.cmd("mix", ~w[check], cd: project_dir, env: %{"MIX_ENV" => "dev"})
+    assert {output, 0} = System.cmd("mix", ~w[check], cd: project_dir, env: %{"MIX_ENV" => "dev"})
 
     assert output =~ "compiler success"
     assert output =~ "formatter success"
